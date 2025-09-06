a110054f4e9cbebcaf22551f88606e30c7175ec6
Thomas Fuller <thomasckfuller@gmail.com>
2025-09-05 22:10:51 -0700

    basics of ch 14

---
+++ value.h
@@ -0,0 +1,21 @@
+#ifndef clox_value_h
+#define clox_value_h
+
+#include "common.h"
+
+typedef double Value;       // Nystrom only supports floats in ch14.
+
+// why does nystrom like specific data structures?
+typedef struct {
+    int capacity;
+    int count;
+    Value* values;
+
+} ValueArray;
+
+void initValueArray(ValueArray* array);
+void writeValueArray(ValueArray* array, Value value);
+void freeValueArray(ValueArray* array);
+void printValue(Value value);
+
+#endif
