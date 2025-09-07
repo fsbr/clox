4bcd635ffd8c2a6054892bb01838dd44918239fa
Thomas Fuller <thomasckfuller@gmail.com>
2025-09-07 05:06:35 -0700

    ended before the trie section

--- scanner.c
+++ scanner.c
@@ -22,6 +22,12 @@ static bool isDigit(char c) {
     return c >= '0' && c  <= '9';           // strings are encoded as ascii so the digits are contained within this range
 }
 
+static bool isAlpha(char c) {
+    return (c >= 'a' && c <= 'z') ||
+           (c >= 'A' && c <= 'Z') ||
+           c == '_';
+}
+
 
 
 static bool isAtEnd() {
@@ -95,6 +101,15 @@ static void skipWhitespace() {
     }
 }
 
+static TokenType identifierType() {
+    return TOKEN_IDENTIFIER;
+}
+
+static Token identifier() {
+    while ( isAlpha(peek()) || isDigit(peek()) ) advance();
+    return makeToken(identifierType());
+}
+
 static Token number() {
     while ( isDigit( peek() ) ) advance();
 
@@ -130,6 +145,7 @@ Token scanToken() {
 
     char c = advance();
 
+    if (isAlpha(c)) return identifier();
     if (isDigit(c)) return number();
 
     switch (c) {
