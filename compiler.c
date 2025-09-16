520cf6dfc521913d2c96581c086d0c39167faaaf
Thomas Fuller <thomasckfuller@gmail.com>
2025-09-15 21:03:47 -0700

    simple if statement is in there;

--- compiler.c
+++ compiler.c
@@ -124,6 +124,13 @@ static void emitBytes(uint8_t byte1, uint8_t byte2) {
     emitByte(byte2);            // byte2, so this function is to help with that.
 }
 
+static int emitJump(uint8_t instruction) {
+    emitByte(instruction);
+    emitByte(0xff);
+    emitByte(0xff);
+    return currentChunk()->count -2;
+}
+
 static void emitReturn() {
     emitByte(OP_RETURN);
 }
@@ -140,6 +147,18 @@ static uint8_t makeConstant(Value value) {
 
 static void emitConstant(Value value) {
     emitBytes(OP_CONSTANT, makeConstant(value));
+}
+
+static void patchJump(int offset) {
+    // -2 to adjust for the bytecode for the jump offset itself
+    int jump = currentChunk()->count - offset - 2;
+
+    if (jump > UINT16_MAX) {
+        error("Too much code to jump over");
+    }
+
+    currentChunk()->code[offset] = (jump >> 8) & 0xff;
+    currentChunk()->code[offset + 1] = jump & 0xff;
 }
 
 static void initCompiler(Compiler* compiler) {
@@ -361,6 +380,17 @@ static void expressionStatement() {
     emitByte(OP_POP);
 }
 
+static void ifStatement() {
+    consume(TOKEN_LEFT_PAREN, "Expect '(' after 'if'.");
+    expression();
+    consume(TOKEN_RIGHT_PAREN, "Expect ')' after 'if'.");
+
+    int thenJump = emitJump(OP_JUMP_IF_FALSE);
+    statement();
+    
+    patchJump(thenJump);
+}
+
 
 static void synchronize() {
     parser.panicMode = false;
@@ -399,6 +429,8 @@ static void declaration() {
 static void statement() {
     if (match(TOKEN_PRINT)) {
         printStatement();
+    } else if (match(TOKEN_IF)) {
+        ifStatement();
     } else if (match(TOKEN_LEFT_BRACE)) {
         beginScope();
         block();
